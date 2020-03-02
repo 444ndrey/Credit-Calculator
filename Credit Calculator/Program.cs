5914dbba4209ff89a8b28b26e30a8e6aff9f0078
teinaava <42085777+teinaava@users.noreply.github.com>
2020-03-02 16:58:54 +0300

    проверка

--- Credit Calculator/Program.cs
+++ Credit Calculator/Program.cs
@@ -17,6 +17,7 @@ namespace Credit_Calculator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //УУУУУУУУУУУУУУУУУУУУУУУУ
         }
     }
 }
